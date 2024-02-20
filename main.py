262dce6aefff902ad2f30dd7ae59c55e88f6a640
carsonadd05 <157738396+carsonadd05@users.noreply.github.com>
2024-02-20 13:39:30 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 #REMOVE PASS AND FIX THE FUNCTION
 #change to test push and forks
 def sum_of_products(list1, list2):
-    pass
+    if len(list1) != len(list2):
+        return "Error: Lists must be the same length."
 
-if __name__ == '__main__':
-   #hello
-    pass
+    result = sum(x * y for x, y in zip(list1, list2))
+    return result
+
+# Test the function
+list1 = [int(x) for x in input("Enter the first series of integers: ")]
+list2 = [int(x) for x in input("Enter the second series of integers: ")]
+
+output = sum_of_products(list1, list2)
+print("Output =", output)
