5528b82bf15f054c78135c8116a40dd09331bd50
carsonadd05 <157738396+carsonadd05@users.noreply.github.com>
2024-02-20 13:44:38 -0600

    main.py
    updaye

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-#REMOVE PASS AND FIX THE FUNCTION
-#change to test push and forks
 def sum_of_products(list1, list2):
     if len(list1) != len(list2):
         return "Error: Lists must be the same length."
@@ -7,9 +5,18 @@ def sum_of_products(list1, list2):
     result = sum(x * y for x, y in zip(list1, list2))
     return result
 
+def get_integer_list(prompt):
+    while True:
+        try:
+            integer_list = [int(x) for x in input(prompt)]
+            return integer_list
+        except ValueError:
+            print("Error: Please enter integers only.")
+
 # Test the function
-list1 = [int(x) for x in input("Enter the first series of integers: ")]
-list2 = [int(x) for x in input("Enter the second series of integers: ")]
+list1 = get_integer_list("Enter the first series of integers: ")
+list2 = get_integer_list("Enter the second series of integers: ")
 
 output = sum_of_products(list1, list2)
 print("Output =", output)
+
